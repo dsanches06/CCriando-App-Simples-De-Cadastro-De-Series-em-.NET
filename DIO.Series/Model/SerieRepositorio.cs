9558a58b5624321f51295de921e22d08238d97ef
Danilson Sanches <dsanches06@outlook.com>
2021-01-21 05:08:38 +0000

    Criação de interface IRepositorio e implementação dessa mesma interface na nova classe SerieRepositorio

---
+++ DIO.Series/Model/SerieRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using DIO.Series.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.Series.Model
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSeriie = new List<Serie>();
+
+        public List<Serie> Listar()
+        {
+            return null;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return null;
+        }
+
+        public void inserir(Serie entidade)
+        {
+
+        }
+
+        public void Excluir(int id)
+        {
+
+        }
+
+        public void Atualizar(int id, Serie entidade)
+        {
+        }
+
+        public int ProximoId()
+        {
+            return 0;
+        }
+    }
+}
