87d014bf8d1619bdb599523dd92785e4fcd8e67d
Danilson Sanches <dsanches06@outlook.com>
2021-01-21 04:50:30 +0000

    Criação de projeto e implementação da classe abstracta EntidadeBase, classe Serie é uma subclasse (herança) de EntidadeBase e criação de Genero do tipo enumerados

---
+++ DIO.Series/Model/Serie.cs
@@ -0,0 +1,45 @@
+﻿using DIO.Series.Model.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.Series.Model
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        public Serie(int Id, Genero Genero, string Titulo, string Descricao, int Ano)
+        {
+            this.Id = Id;
+            this.Genero = Genero;
+            this.Titulo = Titulo;
+            this.Descricao = Descricao;
+            this.Ano = Ano;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            str += "Género: " + this.Genero + Environment.NewLine;
+            str += "Titulo: " + this.Titulo + Environment.NewLine;
+            str += "Descrição: " + this.Descricao + Environment.NewLine;
+            str += "Ano: " + this.Ano;
+            return str;
+        }
+
+        //Emcapsulamento
+        public string GetTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int GetId()
+        {
+            return this.Id;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CCriando-App-Simples-De-Cadastro-De-Series-em-.NET
+# Criando-App-Simples-De-Cadastro-De-Series-em-.NET
 Criando um APP simples de cadastro de séries em .NET
